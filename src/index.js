63296d31f7f5ead60d66020103a2c3d6e5cc0286
taka-123 <63526623+taka-123@users.noreply.github.com>
2021-08-15 18:00:49 +0900

    完了機能実装

--- src/index.js
+++ src/index.js
@@ -20,16 +20,43 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 押された完了ボタンの祖先タグ(li)を未完了リストから削除
+    deleteFromIncompleteList(completeButton.closest("li"));
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.closest("li");
+
+    // TODO内容テキストを取得
+    const text = addTarget.querySelector("p").innerText;
+
+    // divタグの中を初期化
+    const div = addTarget.querySelector(".list-row");
+    div.textContent = null;
+
+    // p生成
+    const p = document.createElement("p");
+    p.innerText = text;
+
+    // butto生成
+    const buckButton = document.createElement("button");
+    buckButton.innerText = "戻す";
+
+    // liタグの子要素に各要素を設定
+    div.appendChild(p);
+    div.appendChild(buckButton);
+    addTarget.appendChild(div);
+
+    // 未完了リストに追加
+    const incompleteList = document.querySelector("#complete-list");
+    incompleteList.appendChild(addTarget);
   });
 
   // button(削除)タグ生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    // 押された削除ボタンの親タグ(div)を未完了リストから削除
-    const deleteTarget = deleteButton.closest("li");
-    document.querySelector("#incomplete-list").removeChild(deleteTarget);
+    // 押された削除ボタンの祖先タグ(li)を未完了リストから削除
+    deleteFromIncompleteList(deleteButton.closest("li"));
   });
 
   // liタグの子要素に各要素を設定
@@ -41,6 +68,11 @@ const onClickAdd = () => {
   // 未完了リストに追加
   const incompleteList = document.querySelector("#incomplete-list");
   incompleteList.appendChild(li);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.querySelector("#incomplete-list").removeChild(target);
 };
 
 const addButton = document.querySelector("#add-button");
